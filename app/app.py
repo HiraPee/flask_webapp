9e95bd7d533ded0c2c6b786fa1ead5d43791d837
HiraPee <yuta.hira-p0119@docomo.ne.jp>
2022-08-28 15:12:18 +0900

    first commit

--- app/app.py
+++ app/app.py
@@ -35,28 +35,5 @@ def post():
 def get():
     return render_template('index.html')
 
-
-@app.route('/ex1/min', methods=['GET'])
-def ex_1():
-    return render_template('ex-1.html')
-
-@app.route('/ex2/max', methods=['GET'])
-def ex_2_get():
-    return render_template('ex-2.html')
-
-@app.route('/ex2/max', methods=['POST'])
-def ex_2_post():
-    print(request.form)
-    data_lists = [request.form['a'],request.form['b'],request.form['c'],request.form['d']]
-    max_data = max(data_lists)
-    if max_data % 2==0:
-        result = {'result':max_data,'waru2':True}
-        return jsonify(result)
-    else :
-        result = {'result':max_data,'waru2':False}
-        return jsonify(result)
-    #return render_template('ex-2.html')
-
-
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True)
